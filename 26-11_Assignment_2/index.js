d7272fc932c9d35afb986d49500c233232715e70
msabhinavchandra <msabhinavchandra@gmail.com>
2024-12-28 23:07:46 +0530

    Just_a_minor_CHange_in_get by id function.

--- 26-11_Assignment_2/index.js
+++ 26-11_Assignment_2/index.js
@@ -74,9 +74,11 @@ app.get('/api/invoices/:id', async (req, res) => {
     try {
         const id = req.params.id;
 
-        const invoice = {
-            invoiceNumber : 1,
-            customerName : 'John Doe'
+        // Retrieve the invoice by ID
+        const invoice = await Invoice.findById(id);
+
+        if (!invoice) {
+            return res.status(404).json({ error: 'Invoice not found' });
         }
 
         res.status(200).json(invoice);
